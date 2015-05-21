ee2298745ffcf0cfb8fa619e7f8116f5b106be0d
Rishabh Rao <rishabhsrao@gmail.com>
2015-05-21 06:15:49 +0530

    Bump to v0.0.1.

---
+++ dist/scripts/app.js
@@ -0,0 +1,1 @@
+define(["jquery"],function(e){"use strict";return{greeting:"Hello world!",version:"0.0.1"}});
---
+++ dist/scripts/main.js
@@ -0,0 +1,1 @@
+!function(e,r,i){"use strict";require.config({paths:{jquery:"../../libraries/jquery/jquery",app:"app",example:"components/example"},shim:{}}),require(["jquery","app"],function(e,r){})}(window,document);
