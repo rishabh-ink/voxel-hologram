61f315c06a205410d993f3b0c201841b18a5a550
Rishabh Rao <rishabhsrao@gmail.com>
2015-05-30 21:13:21 +0530

    Remove wrap fragments.

--- tasks/utils/umd-frag-prelude.js
+++
@@ -1,9 +0,0 @@
-(function(root, factory) {
-  if (typeof define === "function") {
-    define(factory);
-  } else if (typeof exports === "object") {
-    module.exports = factory();
-  } else {
-    root.voxel = factory();
-  }
-}(this, function() {
