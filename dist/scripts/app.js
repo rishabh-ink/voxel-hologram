8fe2a7404e7c4f246059260328abd38fdd808311
Rishabh Rao <rishabhsrao@gmail.com>
2015-05-30 07:48:34 +0530

    Bump to v0.0.4.

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,1 +1,1 @@
-define(["jquery"],function(e){"use strict";return{greeting:"Hello world!",version:"0.0.1"}});
+define(["jquery","components/example"],function(e,n){"use strict";return{greeting:"Hello world!",version:"0.0.1"}});
--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,1 +1,1 @@
-!function(e,r,i){"use strict";require.config({paths:{jquery:"../../libraries/jquery/jquery",app:"app",example:"components/example"},shim:{}}),require(["jquery","app"],function(e,r){})}(window,document);
+!function(e,r,i){"use strict";require.config({paths:{jquery:"../../libraries/jquery/jquery"},shim:{}}),require(["jquery","app"],function(e,r){})}(window,document);
