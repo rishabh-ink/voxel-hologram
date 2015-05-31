c1e4a7cc79c680dcf0ad668e65a209a5fc485804
Rishabh Rao <rishabhsrao@gmail.com>
2015-05-31 11:29:09 +0530

    Remove IIFE and initialization script.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,21 +1,12 @@
-(function(window, document, undefined) {
-  "use strict";
+require.config({
+  paths: {
+    // Libraries
+    "jquery": "../../libraries/jquery/jquery",
+    "hljs": "../../libraries/highlightjs/highlight.pack",
+    "voxel": "../../libraries/voxel/dist/scripts/voxel"
+    // /Libraries
+  },
 
-  require.config({
-    paths: {
-      // Libraries
-      "jquery": "../../libraries/jquery/jquery",
-      "hljs": "../../libraries/highlightjs/highlight.pack",
-      "voxel": "../../libraries/voxel/dist/scripts/voxel"
-      // /Libraries
-    },
-
-    shim: {
-    }
-  });
-
-  require(["jquery", "voxel-hologram"],
-  function( $,        VoxelHologram) {
-    var voxelHologram = VoxelHologram.create();
-  });
-})(window, document);
+  shim: {
+  }
+});
