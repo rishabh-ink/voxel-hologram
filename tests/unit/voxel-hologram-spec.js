40820afc2143a3b3d7645141cb219ebe0421a9bc
Rishabh Rao <rishabhsrao@gmail.com>
2015-05-31 11:29:22 +0530

    Remove unused option.

--- tests/unit/voxel-hologram-spec.js
+++ tests/unit/voxel-hologram-spec.js
@@ -2,9 +2,7 @@ define(["voxel-hologram-fixture", "voxel-hologram"],
 function(VoxelHologramFixture,     VoxelHologram) {
   describe("VoxelHologram", function() {
     it("Should initialize Voxel", function() {
-      var voxelHologram = VoxelHologram.create({
-        useHighlight: false
-      });
+      var voxelHologram = VoxelHologram.create();
 
       expect(voxelHologram.components.voxel).toBeDefined();
     });
